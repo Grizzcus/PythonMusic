f7c1861c8342f25480a687d5431677688e826e4f
Grizzcus <Grizzcus@users.noreply.github.com>
2014-07-06 12:43:21 -0700

    MortalKombatSong.py
    Mortal Kombat theme song in python

---
+++ MortalKombatSong.py
@@ -0,0 +1,354 @@
+import winsound;
+raw_input("Hit Any Key To Begin Playing")
+notes = 0
+beatlength = 230;
+print("Playing Song")
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(294, beatlength) # D
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(330, beatlength) # E
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(294, beatlength) # D
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(330, beatlength) # E
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(330, beatlength) # E
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(247, beatlength) # B
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(294, beatlength) # D
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(349, beatlength) # F
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(349, beatlength) # F
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(349, beatlength) # F
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(247, beatlength) # B
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(349, beatlength) # F
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(247, beatlength) # B
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(294, beatlength) # D
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(330, beatlength) # E
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(294, beatlength) # D
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(330, beatlength) # E
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(330, beatlength) # E
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(247, beatlength) # B
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(294, beatlength) # D
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(349, beatlength) # F
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(349, beatlength) # F
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(349, beatlength) # F
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(349, beatlength) # F
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(247, beatlength) # B
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(330, beatlength) # E
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(330, beatlength) # E
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(391, beatlength) # g
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(262, beatlength) # C
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+notes = notes + 1
+print("%s notes played " % (notes))
+winsound.Beep(220, beatlength) # A
+raw_input("Done!")
